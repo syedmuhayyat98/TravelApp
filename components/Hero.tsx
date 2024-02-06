edab347662375f185fac0a8475461954bc6df263
Syed Azril <syed.azril.muhayyat-smuhammad@edag.com.my>
2024-02-06 17:42:34 +0800

    Working on hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Image from "next/image";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500">
+      <div className="hero-map" />
+
+      {/* LEFT */}
+
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image
+          src="/camp.svg"
+          alt="/camp"
+          width={50}
+          height={50}
+          className="absolute left-[20px] top-[-60px] w-10 lg:w-[50px]"
+        />
+        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]"></p>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
