a00d8802c91b33f22e3524f4bdc97b04555a12d0
Syed Azril <syed.azril.muhayyat-smuhammad@edag.com.my>
2024-02-07 14:45:10 +0800

    Hero component progression

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,8 +15,35 @@ function Hero() {
           height={50}
           className="absolute left-[20px] top-[-60px] w-10 lg:w-[50px]"
         />
-        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
-        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]"></p>
+        <h1 className="bold-52 lg:bold-88">SamOne Camp Area</h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+          We want to be on each of your journeys seeking the satisfaction of
+          seeing the incorruptible beauty of nature. We can help you on an
+          adventure around the world in just one app
+        </p>
+
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  src="/star.svg"
+                  key={index}
+                  alt="start"
+                  width={24}
+                  height={24}
+                />
+              ))}
+          </div>
+
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            999k
+            <span className="regular-16 lg:regular-20 ml-1">
+              Excellent Reviews
+            </span>
+          </p>
+        </div>
       </div>
     </section>
   );
