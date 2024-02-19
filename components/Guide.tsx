ab1b67d50ed1e14a89f29f3009615120df3e99e0
Syed Azril <syed.azril.muhayyat-smuhammad@edag.com.my>
2024-02-19 17:36:13 +0800

    Finish Guide, Features, and GetApp

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Guide() {
   return (
     <section className="flexCenter flex-col">
-      <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+      <div className="padding-container max-container w-full pb-24">
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
         <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
           We are here for you
@@ -21,7 +21,7 @@ function Guide() {
           </p>
         </div>
       </div>
-      <div className="flexCenter max-container relative w-full border-2 border-blue-500">
+      <div className="flexCenter max-container relative w-full">
         <Image
           src="/boat.png"
           alt="boat"
