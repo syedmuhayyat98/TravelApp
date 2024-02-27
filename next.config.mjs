771d1fd85d62498f5c9458d94bce05befabfdb6f
Syed Azril <syed.azril.muhayyat-smuhammad@edag.com.my>
2024-02-27 15:05:08 +0800

    npm run build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // assetPrefix: "/TravelApp/",
-    // basePath: "/TravelApp",
-    // output: "export",
+    assetPrefix: "/TravelApp/",
+    basePath: "/TravelApp",
+    output: "export",
 };
 
 export default nextConfig;
